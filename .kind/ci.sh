402cf34fadc832846bf8f864a3a460091b05e86c
Ryan Faircloth <ryan@dss-i.com>
2023-10-10 14:31:28 -0400

    feat: integration with kyverno.yaml

--- .kind/ci.sh
+++ .kind/ci.sh
@@ -1,19 +1,21 @@
 
-kubectl create namespace kubemod-system || true
-kubectl label namespace kubemod-system admission.kubemod.io/ignore=true --overwrite || true
 kubectl label namespace kube-system topolvm.io/webhook=ignore
 
-helm repo add kubemod https://kubemod.github.io/kubemod-helm
 helm repo add topolvm https://topolvm.github.io/topolvm
+helm repo add kyverno https://kyverno.github.io/kyverno/
 
 helm repo update
 
 helm upgrade --install --create-namespace --namespace cert-manager \
     cert-manager cert-manager/cert-manager --version ${V_CERT_MANAGER:-1.13.1} \
     -f .kind/cert-manager-values.yaml --wait
-helm upgrade --install --namespace kubemod-system \
-    -f .kind/kubenmod-values.yaml \
-    kubemod kubemod/kubemod --wait
+
+helm upgrade --install --create-namespace --namespace kyverno \
+    -f .kind/kyverno-values.yaml \
+    kyverno kyverno/kyverno --wait
+helm upgrade --install --namespace kyverno \
+    -f .kind/kyverno-policies-values.yaml \
+    policies kyverno/kyverno-policies --wait
 helm upgrade --install --create-namespace --namespace topolvm \
     -f .kind/topolvm-values.yaml \
-    topolvm topolvm/topolvm --wait
+    topolvm topolvm/topolvm
