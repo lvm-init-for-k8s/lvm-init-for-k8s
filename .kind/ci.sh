a7c64649e8be164cdb70fc398d9478d227953218
Ryan Faircloth <ryan@dss-i.com>
2023-10-09 20:45:10 -0400

    fix: test scale from 1 to 2

--- .kind/ci.sh
+++ .kind/ci.sh
@@ -1,8 +1,19 @@
 
 kubectl create namespace kubemod-system || true
 kubectl label namespace kubemod-system admission.kubemod.io/ignore=true --overwrite || true
+kubectl label namespace kube-system topolvm.io/webhook=ignore
+
 helm repo add kubemod https://kubemod.github.io/kubemod-helm
-helm repo update kubemod
+helm repo add topolvm https://topolvm.github.io/topolvm
+
+helm repo update
+
+helm upgrade --install --create-namespace --namespace cert-manager \
+    cert-manager cert-manager/cert-manager --version ${V_CERT_MANAGER:-1.13.1} \
+    -f .kind/cert-manager-values.yaml --wait
 helm upgrade --install --namespace kubemod-system \
     -f .kind/kubenmod-values.yaml \
-    kubemod kubemod/kubemod
+    kubemod kubemod/kubemod --wait
+helm upgrade --install --create-namespace --namespace topolvm \
+    -f .kind/topolvm-values.yaml \
+    topolvm topolvm/topolvm --wait
